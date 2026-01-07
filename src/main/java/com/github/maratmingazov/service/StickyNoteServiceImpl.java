e7232c34802a89c0951fad9d625f88583c0d63c3
MaratMingazov <marat@miro.com>
2026-01-07 13:12:17 +0400

    Update Service

--- src/main/java/com/github/maratmingazov/service/StickyNoteServiceImpl.java
+++ src/main/java/com/github/maratmingazov/service/StickyNoteServiceImpl.java
@@ -6,6 +6,7 @@ import com.github.maratmingazov.repository.StickyNoteRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import lombok.val;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Log4j2
@@ -14,6 +15,9 @@ import org.springframework.stereotype.Service;
 public class StickyNoteServiceImpl implements StickyNoteService{
 
     private final StickyNoteRepository stickyNoteRepository;
+
+    @Value("${DEFAULT_COLOR}")
+    private Color defaultColor;
 
     @Override
     public StickyNote getStickyNoteById(String boardKey, Long stickyNoteId) {
@@ -24,7 +28,7 @@ public class StickyNoteServiceImpl implements StickyNoteService{
 
     @Override
     public StickyNote createStickyNote(String boardKey, String text, Integer x, Integer y, Color color) {
-        var stickyNote = stickyNoteRepository.createStickyNote(boardKey, text, x, y, color);
+        var stickyNote = stickyNoteRepository.createStickyNote(boardKey, text, x, y, (color != null) ? color : defaultColor);
         log.info("StickyNote was created with id: {} ", stickyNote.id());
         return stickyNote;
     }
